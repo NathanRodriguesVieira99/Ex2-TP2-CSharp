3cbeeab576080d9d6ffcb1e8eb0259b82f5d7c64
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-03-13 15:22:40 -0300

    feat: ex 2 done

---
+++ tp2-ex2/Ex2.cs
@@ -0,0 +1,29 @@
+﻿class Ex2
+{
+    static void Main()
+    {
+        Console.WriteLine("Informe sua data de nascimento: ");
+
+        string dataInformada = Console.ReadLine();
+
+        if (DateTime.TryParse(dataInformada, out DateTime dataNascimento))
+        {
+            DateTime diaAtual = DateTime.Today;
+
+            DateTime aniversarioData = new DateTime(diaAtual.Year, dataNascimento.Month, dataNascimento.Day);
+
+            if (aniversarioData < diaAtual)
+            {
+                aniversarioData = aniversarioData.AddYears(1);
+            }
+
+            TimeSpan diasQueFaltam = aniversarioData - diaAtual;
+
+            Console.WriteLine($"Faltam {diasQueFaltam.Days} dias para sei aniversario");
+        }
+        else
+        {
+            Console.WriteLine("Data informada invalida");
+        }
+    }
+}
